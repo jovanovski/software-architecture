cd52a79ba82724dd2712ba0fcdb8f5d6fd0a771e
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-16 16:15:54 +0200

    Added sample class and test

--- src/uva/sa/ft1/Hello.java
+++ src/uva/sa/ft1/Hello.java
@@ -1,5 +1,7 @@
 package uva.sa.ft1;
 
 public class Hello {
-
+	public String say() {
+		return "Hello!";
+	}
 }
--- test/uva/sa/ft1/HelloTest.java
+++ test/uva/sa/ft1/HelloTest.java
@@ -4,11 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class HelloTest {
 
 	@Test
 	public void test() {
-		
+		Hello h = new Hello();
+		assertEquals("Hello!", h.say());
 	}
 
 }
