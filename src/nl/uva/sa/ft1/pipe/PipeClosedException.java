103bec6a907e0b6b3d5f5dfd9f028b157985e23d
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-17 13:34:24 +0200

    Added pipeClosedException

---
+++ src/nl/uva/sa/ft1/pipe/PipeClosedException.java
@@ -0,0 +1,10 @@
+package nl.uva.sa.ft1.pipe;
+
+public class PipeClosedException extends OperationFailedException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5794141194112192827L;
+	
+}
