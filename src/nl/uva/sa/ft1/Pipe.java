c0d21ea670c5f0dd1682e2945fd1c631c003a9c5
Merijn Wijngaard <mwijngaard@gmail.com>
2015-09-17 11:34:05 +0200

    Added blocking queue pipe and operationFailedException

--- src/nl/uva/sa/ft1/Pipe.java
+++ src/nl/uva/sa/ft1/Pipe.java
@@ -1,6 +1,8 @@
 package nl.uva.sa.ft1;
 
-interface Pipe<E>{
-    public boolean put(E obj);
-    public E get() throws InterruptedException;
+import nl.uva.sa.ft1.pipe.OperationFailedException;
+
+public interface Pipe<E>{
+    public void put(E obj) throws OperationFailedException;
+    public E get() throws OperationFailedException;
 }
---
+++ src/nl/uva/sa/ft1/pipe/OperationFailedException.java
@@ -0,0 +1,10 @@
+package nl.uva.sa.ft1.pipe;
+
+public class OperationFailedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
